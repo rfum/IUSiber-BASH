ae91c1a41a40c0fb383346e59c8bdbcd64a81a58
rfum <fmataraci6@gmail.com>
2017-02-08 14:24:33 +0400

    Update forbasic.sh

--- loops/forbasic.sh
+++ loops/forbasic.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-
+echo "Yontem 1"
 for arg in {1..5}
 do 
 echo "$arg"
 done 
+echo "Yontem 2"
+for i in 1 2 3 4 5
+do
+   echo "$i"
+done
+echo "Yontem 3"
+# {<başlangıç değeri>..<bitiş değeri>..<artış değeri>}
+for i in {1..5..1}
+do
+echo $i
+done
+echo "Yontem 4"
+for (( c=1; c<=5; c++ ))
+do  
+  echo $c
+done
